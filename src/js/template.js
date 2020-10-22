0fde086f27330b030bed576106de4be528e1185b
OlgaKor23 <work.olga.koryshkina@gmail.com>
2020-10-22 23:16:34 +0300

    reuploaded module 10

---
+++ src/js/template.js
@@ -0,0 +1,1 @@
+import itemTemplate from '../template/item.hbs';import recipes from '../menu.json';const menuRef = document.querySelector('.js-menu');const markup = itemTemplate(recipes);menuRef.insertAdjacentHTML('beforeend', markup);
